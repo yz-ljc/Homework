c5c53949f47aaed0f8b4091c0a688b7960427aae
YZ_Ljc_ <14768819516@163.com>
2025-11-07 23:09:13 +0800

    TUFE

--- Part6/10354.c
+++ Part6/10354.c
@@ -1,40 +1,41 @@
 #include <stdio.h>
 #include <math.h>
 
-double fn(double x)
-{
+double power(double x,double n){
+    double tmp = x;
+    for (int i = 1; i < n; i++) {
+        x *= tmp;
+    }
+    return x;
+}
+double fac(double n){
+    double output = 1;
+    for (int i = 1; i <= n; i++){
+        output = output * i;
+    }
+    return output;
+}
+double fn(double x){
+    double result = 1;
     int n = 2;
-    int correct = 0;
-    double f2 = 1.0;
-    double result = 1.0 + x;
-    while (correct != 1){
-        double f1 = 1;
-        for (int i = 1;i<=n;i++){
-            f1 = f1 * x;
-        }
-        f2 = f2 * n;
+    do{
         if (n % 2 == 0){
-            result = result - f1 / f2;
+            result = result + power(x,n-1) / fac(n-1);
         }
         else{
-            result = result + f1 / f2;
+            result = result - power(x,n-1) / fac(n-1);
         }
         n++;
-        if (fabs(f1/f2) > 1e-8){
-            correct = 0;
-        }
-        else{
-            correct = 1;
-        }
-    }
+    }while (fabs(power(x,n-1) / fac(n-1)) > 1e-8);
     return result;
 }
 
 int main()
 {
-    double x = 0;
+    double x;
     printf("Input x:");
     scanf("%lf",&x);
     double outcome = fn(x);
     printf("1+x-x^2/2!+..+(-1)^(n+1)*x^n/n!=%.6lf",outcome);
+    return 0;
 }
